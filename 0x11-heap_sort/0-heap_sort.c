9ee1fea89e4058eea3395e82c935562b1caeed14
Juansepo13 <3895@holbertonschool.com>
2022-12-08 11:49:07 -0500

    Edit file Task0

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,51 +1,56 @@
 #include "sort.h"
 
-void heapify(int *array, size_t size, long int i, long int s);
-
 /**
- * heap_sort - Function that sorts an array of integers in ascending order
- * using the "sift-down" Heap sort algorithm.
- * @array: Array to be sorted
- * @size: Size of Array
- */
-void heap_sort(int *array, size_t size)
+ * recurisve_heap - helper function to perform recursive heap sort
+ * @array: int array to sort
+ * @size: size of array
+ * @root: parent node
+ * @n: total size of array to be printed
+*/
+void recurisve_heap(int *array, size_t size, size_t root, size_t n)
 {
-	long int i, aux;
+	int temp;
+	size_t largest = root, left = 2 * root + 1, right = 2 * root + 2;
 
-	for (i = (size / 2) - 1; i >= 0; i--)
-		heapify(array, size, i, size);
+	if (left < size && array[left] > array[largest])
+		largest = left;
 
-	for (i = size - 1; i > 0; i--)
+	if (right < size && array[right] > array[largest])
+		largest = right;
+
+	if (largest != root)
 	{
-		aux = array[0];
-		array[0] = array[i];
-		array[i] = aux;
-		print_array(array, size);
-		heapify(array, size, 0, i);
+		temp = array[root];
+		array[root] = array[largest];
+		array[largest] = temp;
+
+		print_array(array, n);
+
+		recurisve_heap(array, size, largest, n);
 	}
 }
-
 /**
- * heapify - order the tree - max heap
- * @array: Array to be sorted
- * @size: Size of Array
- * @i: index to start ordering the heap
- * @s: new size for ordering the heap
- */
-void heapify(int *array, size_t size, long int i, long int s)
+ * heap_sort - function that sorts an array of integers in
+ * ascending order using the Heap sort algorithm
+ * @array: int array to sort
+ * @size: size of array
+*/
+void heap_sort(int *array, size_t size)
 {
-	long int j, max = i, aux;
+	int i, temp;
 
-	for (j = 1; j < 3 && s > (2 * i) + j; j++)
-		if (array[i] < array[(2 * i) + j] && array[max] < array[(2 * i) + j])
-			max = (2 * i) + j;
+	for (i = size / 2 - 1; i >= 0; i--)
+		recurisve_heap(array, size, i, size);
 
-	if (max != i)
+	for (i = size - 1; i >= 0; i--)
 	{
-		aux = array[i];
-		array[i] = array[max];
-		array[max] = aux;
-		print_array(array, size);
-		heapify(array, size, max, s);
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+
+		if (i != 0)
+			print_array(array, size);
+
+		recurisve_heap(array, i, 0, size);
 	}
 }
