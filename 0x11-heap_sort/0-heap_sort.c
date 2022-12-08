35b31763535830b785005da3c2871e81278eeb15
Juansepo13 <juancho_s1314@hotmail.com>
2022-12-08 17:35:18 -0500

    add all files

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,119 +1,75 @@
 #include "sort.h"
 
 /**
- * get_parent_index - gets the index of the parent of a node
- *
- * @child_index: the index of the child
- *
- * Return: the index of the parent
- */
-size_t get_parent_index(size_t child_index)
-{
-	return ((child_index - 1) / 2);
-}
-
-/**
- * swap - swaps two elements in an array
- *
- * @prmArray: the array to be sorted
- * @prmSize: the size of the array
- * @prmIndex1: the index of the first element
- * @prmIndex2: the index of the second element
- */
-void swap(int *prmArray, size_t prmSize, size_t prmIndex1, size_t prmIndex2)
-{
-	int temp = prmArray[prmIndex1];
-
-	prmArray[prmIndex1] = prmArray[prmIndex2];
-	prmArray[prmIndex2] = temp;
-	print_array(prmArray, prmSize);
-}
-
-/**
- * sift_down - fixes a heap
- *
- * @prmArray: the heap to fix
- * @prmRoot: the root of the heap
- * @prmEnd: the last index of the heap
- * @prmSize: size of the array
- *
- * Return: void
- */
-void sift_down(int *prmArray, size_t prmRoot, size_t prmEnd, size_t prmSize)
-{
-	size_t left_child, right_child, current, root;
-
-	root = prmRoot;
-
-	while ((left_child = (2 * root) + 1) <= prmEnd)
-	{
-		current = root;
-		right_child = left_child + 1;
-
-		/**
-		 * if the left child is greater than the current node,
-		 *  then the current node is the left child
-		 */
-		if (prmArray[current] < prmArray[left_child])
-			current = left_child;
-
-		/**
-		 * if the right child is greater than the current node,
-		 * then the current node is the right child
-		 */
-		if (right_child <= prmEnd && prmArray[current] < prmArray[right_child])
-			current = right_child;
-
-		if (current == root)
-			return;
-
-		/** Swap the values */
-		swap(prmArray, prmSize, root, current);
-		root = current;
-	}
-}
-
-/**
- * make_heap - makes a heap from an unsorted array
- *
- * @array: array to turn into a heap
- * @size: size of the array
- *
- * Return: void
- */
-void make_heap(int *array, size_t size)
-{
-	size_t parent;
-
-	for (parent = get_parent_index(size - 1); 1; parent--)
-	{
-		sift_down(array, parent, size - 1, size);
-
-		if (parent == 0)
-			break;
-	}
-}
-
-/**
- * heap_sort - Sorts an array using the heap sort algorithm
- *
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * heap_sort - Build max heap
+ * @array: array
+ * @size: size of array
  */
 void heap_sort(int *array, size_t size)
 {
-	size_t end;
 
-	if (array == NULL || size < 2)
+	size_t i;
+
+	if (array == NULL)
 		return;
 
-	make_heap(array, size);
-	end = size - 1;
+	/* Build max heap */
+	for (i = size / 2 ; i > 0; i--)
+		heap_root(array, size, i - 1, size);
 
-	while (end > 0)
+	/* Heap sort */
+	for (i = size - 1; i > 0; i--)
 	{
-		swap(array, size, end, 0);
-		end--;
-		sift_down(array, 0, end, size);
+		swap(&array[0], &array[i], array, size);
+
+		/* Heapify root element to get highest element at root again */
+		heap_root(array, i, 0, size);
 	}
+}
+
+
+/**
+ * heap_root - Find largest among root, left child and right chil
+ * @arr: array
+ * @n: size array
+ * @i: current position
+ * @size: size
+ */
+void heap_root(int *arr, int n, int i, size_t size)
+{
+	/* Find largest among root, left child and right child */
+
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < n && arr[right] > arr[largest])
+		largest = right;
+
+	/* Swap and continue heapifying if root is not largest */
+	if (largest != i)
+	{
+		swap(&arr[i], &arr[largest], arr, size);
+		heap_root(arr, n, largest, size);
+	}
+
+}
+
+/**
+ * swap - Function to swap the the position of two elements
+ * @a: first integer
+ * @b: second integer
+ * @array: array of numbers
+ * @n: size of array
+ */
+void swap(int *a, int *b, int *array, size_t n)
+{
+
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	print_array(array, n);
 }
--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,13 +1,11 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H_
+#define _SORT_H_
 
+#include <stdio.h>
 #include <stdlib.h>
 
-size_t get_parent_index(size_t child_index);
-void swap(int *prmArray, size_t prmSize, size_t prmIndex1, size_t prmIndex2);
-void sift_down(int *prmArray, size_t prmRoot, size_t prmEnd, size_t prmSize);
-void make_heap(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void swap(int *a, int *b, int *array, size_t n);
+void heap_root(int *arr, int n, int i, size_t size);
 void heap_sort(int *array, size_t size);
-
-#endif /** SORT_H **/
+#endif /* _SORT_H_ */
